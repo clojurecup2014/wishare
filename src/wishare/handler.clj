2582e4003e6dcb3cc90e45053c17060e8f0ea408
Astynax84 <astynax84@gmail.com>
2014-09-27 05:14:05 +0400

    file logging, configuration file

--- src/wishare/handler.clj
+++ src/wishare/handler.clj
@@ -1,14 +1,20 @@
 (ns wishare.handler
-  (:require [compojure.core :refer :all]
+  (:require [carica.core :refer [config]]
+            [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [taoensso.timbre :as timbre]
             [prone.middleware :as prone]))
 
+
+(def debug? (config :debug?))
+
+
 (defroutes app-routes
   (GET "/" [] (slurp "resources/public/index.html"))
   (route/resources "/")
   (route/not-found "Not Found"))
+
 
 (defn with-logging
   "Exception logging middleware"
@@ -20,13 +26,26 @@
         (timbre/error e)
         (throw e)))))
 
+
 (def app
-  (-> app-routes
-      with-logging
-      prone/wrap-exceptions
-      handler/site))
+  (->
+   (cond-> app-routes
+           debug? prone/wrap-exceptions)
+   with-logging
+   handler/site))
+
 
 (defn init
   "App initialization"
   []
-  (println "Started!"))
+  (timbre/info "Started!")
+
+  (when debug?
+    (timbre/info "Debug mode: ON"))
+
+  ;; file logging
+  (when (config :log-to-file?)
+    (let [filename (config :log-file-name)]
+      (timbre/info (str "Log file: \"" filename "\""))
+      (timbre/set-config! [:appenders :spit :enabled?] true)
+      (timbre/set-config! [:shared-appender-config :spit-filename] filename))))
