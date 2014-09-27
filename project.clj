1e742b07e9c135404c2ea55f3db084951938e6d8
Rinat Sabitov <rinat.sabitov@gmail.com>
2014-09-27 15:33:53 +0400

    more changes for CI

--- project.clj
+++ project.clj
@@ -44,6 +44,7 @@
   :profiles {:dev
              {:plugins [[lein-datomic "0.2.0"]]
               :datomic {:config "resources/datomic/free-transactor-template.properties"
-                        :db-uri "datomic:free://localhost:4334/wishare-db"}
+                        :db-uri "datomic:free://localhost:4334/wishare-db"
+                        :install-location "/home/rinat/datomic-free-0.9.4899"}
               :dependencies [[javax.servlet/servlet-api "2.5"]]}
              :uberjar {:aot [wishare.main]}})
