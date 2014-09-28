43a1b097e0a4fd7b759e407a8a7522dea3f03976
Mendor <mendor@yuuzukiyo.net>
2014-09-28 13:36:25 +0300

    eBay support for images links

--- src/wishare/utils.clj
+++ src/wishare/utils.clj
@@ -5,7 +5,9 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(def image-paths {"amazon." [:div.imgTagWrapper :img]})
+; Enlive selectors syntax can be found at https://github.com/cgrand/enlive#selector-syntax
+(def image-paths {"amazon." [:div.imgTagWrapper :img]
+                  "ebay.com" [:td.img300 :img#icImg]})
 
 (defn product-picture-url
   "Parse Amazon/eBay/etc page and return product image URL if available"
@@ -16,6 +18,6 @@
               page-content (fetch-url url)]
           (((first (html/select page-content (image-paths %))) :attrs) :src))
         ; TODO: logging for failed HTTP queries
-        (catch Exception e nil))
+        (catch Exception e "Error"))
       nil)
     (keys image-paths))))
